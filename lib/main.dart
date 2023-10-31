3767fca5a850a3f4456c2d4ffe774dd93d9f13d7
itamarpambo <itamarpambo751@gmail.com>
2023-10-31 17:49:26 +0100

    A dicionando texto pessoas e pode entrar, estilo e cor

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,22 @@ import 'package:flutter/material.dart';
 
 
 void main() async {
-  
-  runApp(MaterialApp(
-    title: "Calculadora de IMC",
-    home: Container(color: Colors.black,),
+  runApp(const MaterialApp(
+    title: "Contador de Pessoas",
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget> [
+        Text("Pessoas: 0", 
+          style: TextStyle(color: Colors.blue),
+        ),
+        Text("Pode entrar", 
+          style: TextStyle(
+            color: Colors.blue, 
+            fontStyle: FontStyle.italic,
+            fontSize: 30.0
+          ),
+        )
+      ],
+    ),
   ));
 }
